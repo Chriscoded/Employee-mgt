193ca22360ab9734a5ab2c430b24c2e50344f188
chriscoded <amaemechris@gmail.com>
2023-10-18 14:01:20 +0100

    initial commit

---
+++ app/Models/User.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'username','first_name', 'last_name', 'role', 'email',
+        'password','phone','address','gender','dob','join_date','job_type','city','salary','age',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+//    For counting the leave
+    public function leave()
+    {
+        return $this->HasMAny('App\Leave','employee_id');
+    }
+
+    public function get_UserNumber(){
+
+        $this->db->select("count(*) as no");
+        $query = $this->db->get("users");
+        return $query->result();
+
+    }
+}
